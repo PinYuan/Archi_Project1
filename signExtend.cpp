e49ec674fd5398e652a7c72dad57e092908b2cb3
pin_yuan <c0919156850@gmail.com>
2017-03-12 00:42:34 +0800

    complete signExtend func

---
+++ signExtend.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bitset<32> signExtend(bitset<16> dataIn){
+    bitset<32> dataOut(dataIn.to_ulong());
+    if(dataIn[15]){//negative
+        for(int i=31;i>=16;i--)
+            dataOut[i] = 1;
+    }
+    return dataOut;
+}
+
