078345c69185c09d953cf6fb575a6a252919743c
pin_yuan <c0919156850@gmail.com>
2017-03-15 23:18:05 +0800

    add header file

--- regFile.h
+++ regFile.h
@@ -1,16 +1,19 @@
 #ifndef REGFILE_H_INCLUDED
 #define REGFILE_H_INCLUDED
+#include <bits/stdc++.h>
+using namespace std;
 
 class RegisterFile{
 private:
     bitset<32> registers[32];
-    bitset<32> readData1, readData2;
     bitset<32> oldRegisters[32];
     bitset<32> oldPc, oldHI, oldLO;
 public:
-    RegisterFile();
+    bitset<32> readData1, readData2;
+    
+	RegisterFile();
 
-    void readWrite(bitset<5> readRegigter1, bitset<5> readRegigter2, bitset<5> writeRegigter
+    void readWrite(bitset<5> readRegister1, bitset<5> readRegister2, bitset<5> writeRegister
                    , bitset<32> writeData, bitset<1> regWrite);
 
     void outputRegister(int cycle);
