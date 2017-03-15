61f6fffd33a82cf1d8b882f5f154774cb7d93f14
pin_yuan <c0919156850@gmail.com>
2017-03-15 22:52:17 +0800

    try file

--- instruction.cpp
+++ instruction.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
+#include "instruction.h"
 using namespace std;
 
-Instruction::Instruction(bitset<32> inst){
-	completeInst = inst;
+Instruction::Instruction(bitset<32> instruction){
+	completeInst = instruction;
     for(int i=0;i<6;i++)
     	opCode.set(i, instruction[26+i]);
     for(int i=0;i<5;i++)
@@ -13,10 +14,10 @@ Instruction::Instruction(bitset<32> inst){
         regRd.set(i, instruction[11+i]);
     for(int i=0;i<5;i++)
         shamt.set(i, instruction[6+i]);
-    for(int i=0;i<5;i++)
+    for(int i=0;i<6;i++)
         func.set(i, instruction[i]);
     for(int i=0;i<16;i++)
-        immediate.set(i, instruction[16+i]);
+        immediate.set(i, instruction[i]);
     for(int i=0;i<26;i++)
         targetAddr.set(i, instruction[i]);
     if(opCode == 63)
